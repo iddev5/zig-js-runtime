72d50f3373a4f294612df46b677b5c2198013ece
iddev5 <ayushbardhan5@gmail.com>
2022-07-03 12:50:38 +0530

    Update function example to use webgpu
    Needs to be run on firefox nightly or chrome canary

--- examples/func.zig
+++ examples/func.zig
@@ -1,10 +1,20 @@
 const std = @import("std");
 const js = @import("js-runtime");
 
-pub fn main() !void {
-    if (true) unreachable;
+fn zigFunc(args: js.Object, _: u32) js.Value {
+    const val1 = args.getIndex(0);
+    _ = js.global().get("console").value(.object, null).call("log", &.{val1});
+    return js.Value{ .tag = .num, .val = .{ .num = 20.2 } };
+}
 
+pub fn main() !void {
     const global = js.global();
-    const xyz = global.get("xyz").value(.func, null);
-    global.set("xyz_result", xyz.invoke(&.{}));
+    const func = js.createFunction(zigFunc);
+
+    const navigator = global.get("navigator").value(.object, null).get("gpu").value(.object, null);
+    const promise = navigator.call("requestAdapter", &.{});
+    _ = promise.value(.object, null).call("then", &.{func.toValue()});
+
+    _ = func.invoke(&.{js.createNumber(10.1)});
+    global.set("func", func.toValue());
 }
